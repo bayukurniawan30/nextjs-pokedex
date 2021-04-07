d00d6dbea7e235b49eab2b7b4241222a156c93c3
baycoretech <creatifycms@gmail.com>
2021-04-07 11:41:55 +0800

    Update detail page

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -7,29 +7,29 @@ function Icon(stat) {
     if (stat == 'hp') {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
         )
     }
     else if (stat == 'attack' || stat == 'special-attack') {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
             </svg>
         )
     }
     else if (stat == 'defense' || stat == 'special-defense') {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
             </svg>
         )
     }
     else if (stat == 'speed') {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
             </svg>
         )
     }
@@ -62,7 +62,7 @@ function Detail({ data }) {
                             <div className="lg:col-start-2 md:pl-20">
                                 <p className="text-indigo-500 text-md font-medium capitalize">
                                     {data.types.map(({ type, slot }) => (
-                                        <span>{type.name} {typesLength === slot ? "" : ' - '}</span>
+                                        <span key={slot}>{type.name} {typesLength === slot ? "" : ' - '}</span>
                                     ))}
                                 </p>
                                 <h4 className="text-2xl leading-8 font-extrabold text-gray-900 dark:text-white tracking-tight sm:leading-9 capitalize">
@@ -70,7 +70,7 @@ function Detail({ data }) {
                                 </h4>
                                 <ul className="mt-10">
                                     {data.stats.map(({ stat, base_stat }) => (
-                                    <li className="mb-6">
+                                    <li key={stat.name} className="mb-6">
                                         <div className="flex">
                                             <div className="flex-shrink-0">
                                                 <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
