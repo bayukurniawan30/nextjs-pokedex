9d7c757833b50dce0d797611f479efe61f873528
baycoretech <creatifycms@gmail.com>
2021-05-11 13:07:12 +0800

    Update loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import "../css/style.css";
 import Head from "next/head";
+import Router from 'next/router';
 import Layout from "@components/layout";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -14,30 +14,30 @@ function IndexPage({ allData, moreData }) {
 					<div className="grid grid-cols-1 md:grid-cols-4 gap-8">
 						{/* Looping untuk menampilkan data pokemon */}
 						{allData.map(({ id, name, sprites, types }) => (
-						<div key={name}>
-							<div className="overflow-hidden shadow-lg hover:shadow-xl transition duration-500 ease-in-out rounded-lg h-90 cursor-pointer m-auto">
-								<a href="#" className="w-full block h-full">
-									<Link href={"/detail/" + id}>
-										<a><img alt={name} src={sprites.front_default} className="max-h-60 " width="100%"/></a>
-									</Link>
-									<div className="bg-white dark:bg-gray-800 w-full p-4">
-										<p className="text-indigo-500 text-md font-medium capitalize">
-											{types[0].type.name}
-										</p>
-										<p className="text-gray-800 dark:text-white text-xl font-medium mb-2 capitalize">
-											<Link href={"/detail/" + id}>
-												<a>
-													{name}
-												</a>
-											</Link>
-										</p>
-										<p className="text-gray-400 dark:text-gray-300 font-light text-md">
-											{moreData[id - 1].flavor_text_entries[0].flavor_text}
-										</p>
+							<div key={name}>
+								<div className="overflow-hidden shadow-lg hover:shadow-xl transition duration-500 ease-in-out rounded-lg h-90 cursor-pointer m-auto">
+									<div className="w-full block h-full">
+										<Link href={"/detail/" + id}>
+											<a><img alt={name} src={sprites.front_default} className="w-80 " width="100%" /></a>
+										</Link>
+										<div className="bg-white dark:bg-gray-800 w-full p-4">
+											<p className="text-indigo-500 text-md font-medium capitalize">
+												{types[0].type.name}
+											</p>
+											<p className="text-gray-800 dark:text-white text-xl font-medium mb-2 capitalize">
+												<Link href={"/detail/" + id}>
+													<a>
+														#{id} {name}
+													</a>
+												</Link>
+											</p>
+											<p className="text-gray-400 dark:text-gray-300 font-light text-md">
+												{moreData[id - 1].flavor_text_entries[0].flavor_text}
+											</p>
+										</div>
 									</div>
-								</a>
+								</div>
 							</div>
-						</div>
 						))}
 					</div>
 				</div>
@@ -46,32 +46,32 @@ function IndexPage({ allData, moreData }) {
 	);
 }
 
-// fetch data dengan getStaticProps
-export async function getStaticProps() {
+// fetch data dengan getServerSideProps
+export async function getServerSideProps() {
 	// cari data pokemon
 
 	// set limitnya brapa
 	const limit = 151
 	// fetch pokemon API
-	const res  = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
 	// ubah data ke format json
 	const data = await res.json()
-	const allData  = []
+	const allData = []
 	const moreData = []
 
 	// masukkan data ke array yang baru
-	let count  = 0
+	let count = 0
 	let count2 = 0
 	for (const item of data.results) {
-		const pokeUrl  = await fetch(item.url);
+		const pokeUrl = await fetch(item.url);
 		const pokeData = await pokeUrl.json();
 
-		const speciesUrl   = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeData.name}`)
-		const speciesData  = await speciesUrl.json()
-		allData[count++]   = pokeData
+		const speciesUrl = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeData.name}`)
+		const speciesData = await speciesUrl.json()
+		allData[count++] = pokeData
 		moreData[count2++] = speciesData
 	}
-  
+
 	// pass data ke IndexPage
 	return {
 		props: {
